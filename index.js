c82d53e5ad2eda85d45744737f728f2390d4d169
Arav Narula <arav.1tb@gmail.com>
2025-03-01 19:53:16 -0600

    Serial Port :D

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const { SerialPort } = require('serialport');
 const cors = require('cors');
 app.use(cors());
+var expressWs = require('express-ws')(app);
+
 
 app.use(express.json());
 
+// SerialPort.list().then(ports => {
+//     ports.forEach(port => {
+//         console.log(port.path);
+//     });
+// }).catch(err => {
+//     console.error('Error listing ports', err);
+// });
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function rightBlink() {
+    for (let i = 0; i < 5; i++) {
+        const sendB = pico.write('B', err => {
+            if (err) {
+                return console.log('Error on write: ', err.message);
+            }
+            console.log('B');
+        })
+      await sleep(1000)
+        console.log(sendB)      
+        const sendF = pico.write('F', err => {
+            if (err) {
+                return console.log('Error on write: ', err.message);
+            }
+            console.log('F');
+        });
+        console.log(sendF)       
+        await sleep(1000)
+    }
+}
+
+
+const pico = new SerialPort({
+    path: '/dev/tty.usbmodem1301',
+    baudRate: 9600,
+})
+
 app.get('/right', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
+app.post('/right', async (req, res) => {
+    await rightBlink();
 
-app.post('/right', (req, res) => {
-    console.log(req.body)
 })
+
 
 app.get('/left', (req, res) => {
     res.send('Hello World!')
-  })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
